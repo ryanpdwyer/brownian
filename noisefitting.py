9911505702a8b36bd48438d54098c37fb6da1bbf
ryanpdwyer <devnull@localhost>
2013-11-22 18:32:14 -0500

    Added tests to initialization of plotting object.

--- noisefitting.py
+++ noisefitting.py
@@ -6,7 +6,8 @@ Ryan Dwyer
 This document will implement John Marohn's Brownian motion noise 
 fitting routine in Python. The file has the following stuructre.
 
-class 
+    class BrownianMotionFitter
+        This is the main object that stores, fits, and plots the data.
 
 """
 
@@ -30,7 +31,7 @@ class BrownianMotionFitter(object):
 
     """
 
-    def __init__(self, f, PSD, estimates):
+    def __init__(self, f, PSD, T, estimates):
         """
         f
             frequency (Hz)
@@ -44,39 +45,58 @@ class BrownianMotionFitter(object):
         """
         self.f = f
         self.PSD_raw = PSD
-        self.estimates = estimates
-        self.T = T
+        self.T = T * u.K
+        
+        missing = [key for key in ['f_c', 'k_c', 'Q'] if key not in estimates] 
+        if len(missing) == 0:
+            self.estimates = estimates
+        else:
+            raise ValueError("The dictionary 'estimates' is missing keys:\
+                                            {missing}".format(missing=missing))
 
     def guess_P_detector(self):
-        self.P_detector0 = np.percentile(self.PSD, 2)
+        self.P_detector0_raw = np.percentile(self.PSD, 2)
 
     def scale_data(self):
         self.PSD = self.PSD_raw / self.P_detector0
+        self.P_detector0 = 1
 
 
-    def calc_fit(self, f_min = 1e3, f_max = 1e5):
-        self.f_min = f_min
-        self.f_max = f_max
-
-        self.guess_P_detector()
-        self.scale_data()
+    #def calc_fit(self, f_min = 1e3, f_max = 1e5):
+    #    self.f_min = f_min
+    #    self.f_max = f_max
+    #
+    #    self.guess_P_detector()
+    #    self.scale_data()
+    #    
+    #    self._first_pass()
+    #    self._second_pass()
+    #    self._final_passes()
 
     def calc_initial_params(self):
-        f_c = estimates['f_c'] * u.Hz
-        k_c = estimates['k_c'] * u.N / u.m
-        Q = estimates['Q']
+        f_c = self.estimates['f_c'] * u.Hz
+        k_c = self.estimates['k_c'] * u.N / u.m
+        Q = self.estimates['Q']
 
-        P_x0guess = (P_x0(f_c, k_c, Q, self.T) / ( P_detector0 * u.nm)
-        self.inital_params = [P_x0guess, f_c, Q]
+        P_x0guess = (P_x0(f_c, k_c, Q, self.T) / 
+                            (self.P_detector0 * u.nm ** 2 / u.Hz))
+        
+        self.inital_params = [P_x0guess.magnitude, f_c.magnitude, Q.magnitude]
 
     def _first_pass(self):
+        """The first pass of the fitting protocol. We use user input guesses for
+        the three cantilever parameters, and fix the thermal noise floor to a 
+        low but reasonable guess. Then, we fit the remaining three parameters
+        (P_x0, f_c, Q). The output of this fit is saved to 
+        self.popt1, self.pcov1.
+        """
         
         self.calc_initial_params()
 
         def Pf_fixed_P_detector(f, P_x0, f_c, Q):
-            return Pf(f, P_x0, f_c, Q, self.P_detector0)\
+            return Pf(f, P_x0, f_c, Q, self.P_detector0)
 
-         self.popt1, self.pcov1 = curve_fit(Pf_fixed_P_detector,
+        self.popt1, self.pcov1 = curve_fit(Pf_fixed_P_detector,
                                     self.f, self.PSD,
                                     p0=self.initial_params,
                                     sigma=self.PSD * 0.1 # Make this real
@@ -105,8 +125,7 @@ def Pf(f, P_x0, f_c, Q, P_detector):
 
     P_detector
         The detector noise floor.
-        """"
+        """
     return  (P_x0 * f_c**4 / 
                     ((f**2 - f_c**2)**2 + f**2 * f_c**2 / Q**2)
                                                              + P_detector)
-
--- test_noisefitting.py
+++ test_noisefitting.py
@@ -1,4 +1,31 @@
-from noisefitting import *
+from noisefitting import BrownianMotionFitter, u
+import pandas as pd
+import numpy as np
+import os
+import matplotlib as mpl
+from nose.tools import assert_raises
+
+os.chdir(r'C:\Users\as-chm-marohninst\Dropbox\Python Code\projects\NoiseFitting')
+data = pd.read_pickle('data.pkl')
+f = data.f.values
+PSDx = data.PSDx.values
+estimates = {'f_c':63700, 'k_c':3.5, 'Q':20000}
+T = 295
+
+def test_NoiseFitter_init():
+    # Try a case that should work.
+    reduced_f = f[::25000]
+    reduced_PSD = PSDx[::25000]
+    a = BrownianMotionFitter(reduced_f, reduced_PSD, T, estimates)
+    assert np.all(a.f == reduced_f)
+    assert np.all(a.PSD_raw == reduced_PSD)
+    assert np.all(a.T == T * u.K) 
+    assert a.estimates == estimates
+    # Try a case that should fail.
+    bad_estimates = {'f_c':63700, 'k_c':3.5}
+    assert_raises(ValueError, BrownianMotionFitter,
+                            reduced_f, reduced_PSD, T, bad_estimates)
+    
 
 def test_Px0():
 	pass
