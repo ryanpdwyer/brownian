ee2c20c99582e38eb42efc23ea8628caead01d49
Ryan Dwyer <ryanpdwyer@gmail.com>
2014-07-14 13:23:47 -0400

    Cleaned up setup file, renamed tests.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from disutils.core import setup
 
 setup(name='noisefitting',
-	  description=""
-	  version="0.1dev",
-	  install_requires=[
-	  'numpy', 'scipy', 'matplotlib', 'pint', 'pandas'
-	  ])
+      description="",
+      version="0.1dev",
+      install_requires=[
+      'numpy', 'scipy', 'matplotlib', 'pint', 'pandas', 'nose'
+      ])
