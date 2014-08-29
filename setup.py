06534f639151f4d0efbe82b9d106a99830619346
Ryan Dwyer <ryanpdwyer@gmail.com>
2014-08-28 22:38:55 -0400

    Switched to the proper directory structure

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
-from disutils.core import setup
+#!/usr/bin/env python
+# Enables the package to be used in develop mode
+# See http://pythonhosted.org/setuptools/setuptools.html#development-mode
+# See https://github.com/scikit-learn/scikit-learn/issues/1016
+try:
+    import setuptools
+except ImportError:
+    pass
+
+from distutils.core import setup
 
 setup(name='noisefitting',
       description="",
       version="0.1dev",
+      author='Ryan Dwyer',
+      author_email='ryanpdwyer@gmail.com',
+      packages=['brownian'],
       install_requires=[
       'numpy', 'scipy', 'matplotlib', 'pint',
       'nose', 'cython', 'h5py'
