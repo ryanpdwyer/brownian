45e7a9b5990ca89872297e34d64bc61fae4832af
Ryan Dwyer <ryanpdwyer@gmail.com>
2014-09-18 21:48:03 -0400

    Trying django-style setup.py"

--- setup.py
+++ setup.py
@@ -9,12 +9,24 @@ except ImportError:
 
 from distutils.core import setup
 
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    See https://github.com/un33k/django-uuslug/blob/master/setup.py.
+    Using this to auotmatically find all packages.
+
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
 setup(name='brownian',
       description="",
+      url='https://github.com/ryanpdwyer/brownian',
       version="0.1dev",
       author='Ryan Dwyer',
       author_email='ryanpdwyer@gmail.com',
-      packages=['brownian'],
+      packages=get_packages('brownian'),
       install_requires=[
       'numpy', 'scipy', 'matplotlib', 'pint',
       'nose', 'h5py', 'uncertainties'
