769d8edd09b5a17caf37d37a63d9fa1f4b898d5c
Ryan Dwyer <ryanpdwyer@gmail.com>
2016-07-20 17:02:23 -0400

    Allow choice of model

--- brownian/_stan.py
+++ brownian/_stan.py
@@ -264,6 +264,45 @@ model {
             + Pdet)
             );
 }
+""",
+'gamma2': """
+data {
+  int<lower=0> N;
+  int<lower=0> M;
+  vector[N] f;
+  vector[N] y;
+  real mu_fc;
+  real mu_kc;
+  real mu_Q;
+  real mu_Pdet;
+  real sigma_fc;
+  real sigma_kc;
+  real sigma_Q;
+  real sigma_Pdet;
+  real scale;
+  real<lower=0> T;
+}
+parameters {
+  real dfc;
+  real<lower=0> kc;
+  real<lower=0> Q;
+  real<lower=0> Pdet;
+}
+model {
+    # Priors on fit parameters
+    dfc ~ normal(0, sigma_fc);
+    kc ~ normal(mu_kc, sigma_kc);
+    Q ~ normal(mu_Q, sigma_Q);
+    Pdet ~ cauchy(mu_Pdet, sigma_Pdet);
+    
+    for (i in 1:N) {
+        y[i] ~ gamma(M, M / (
+            ((2 * 1.381e-5 * T) / (pi() * Q * kc)) / scale * (dfc + mu_fc)^3 /
+            ((f[i] * f[i] - (dfc + mu_fc)^2) * (f[i] * f[i] - (dfc + mu_fc)^2) + f[i] * f[i] * (dfc + mu_fc)^2 / Q^2)
+            + Pdet)
+            );
+    }
+}
 """
 }
 
@@ -791,10 +830,11 @@ FMAX                Max frequency to fit [Hz]
 @click.option('--resonance-frequency-stdev', '-sf', default=5., help='Resonance frequency prior standard deviation')
 @click.option('--chains', '-c', default=4, help="MCMC chains to run (4)")
 @click.option('--iterations', '-i', default=1000, help='MCMC iterations per chain')
+@click.option('--model', '-m', default='gamma', help='Model to fit.')
 def bayesian_brownian_cli(filename, fmin, fmax, output, spring_constant,
                           quality_factor, temperature, spring_constant_stdev,
                           quality_factor_stdev, resonance_frequency_stdev,
-                          chains, iterations):
+                          chains, iterations, model):
     kc = spring_constant
     Q = quality_factor
     T = temperature
@@ -808,7 +848,7 @@ def bayesian_brownian_cli(filename, fmin, fmax, output, spring_constant,
     # Need an option to use a real file here.
     fhsamp = h5py.File('.inmem.h5', driver='core', backing_store=False)
 
-    b = BayesianBrownian('gamma', data, priors={})
+    b = BayesianBrownian(model, data, priors={})
 
     b.sample(chains=chains, iter=iterations)
     b.save(fhsamp)
