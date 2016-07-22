dc943c6fa374438dd09f0342d486921c9eeda94f
Ryan Dwyer <ryanpdwyer@gmail.com>
2016-07-22 14:04:41 -0400

    Changed integration test to single core: runs successfully on Windows

--- brownian/tests/test_bayes.py
+++ brownian/tests/test_bayes.py
@@ -10,10 +10,10 @@ class testBayes(unittest.TestCase):
         self.fh = h5py.File('../ex/brownian173033.h5', 'r')
     def test_integration(self):
         """Integration test: data extraction, sampling, and creation of plotting object."""
-        d = bayes.fh2data(self.fh, 70511, 70611, 3.5, 20000,
+        d = bayes.fh2data(self.fh, 70521, 70601, 3.5, 20000,
               sigma_Q=15000, Pdet=1e-8,
               sigma_Pdet=3e-8, sigma_kc=2.5)
-        traces = bayes.sample_pymc3(d, samples=10, njobs=2)
+        traces = bayes.sample_pymc3(d, samples=10, njobs=1)
         ppb = bayes.PlotPyMCBrownian(d, traces, 'test-data')
 
     def tearDown(self):
