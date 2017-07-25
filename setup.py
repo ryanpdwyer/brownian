2552394d5483cd26a48fdd3fa54ed251c4a3a02e
Ryan Dwyer <ryanpdwyer@gmail.com>
2017-07-25 00:38:05 -0400

    Added package_data

--- setup.py
+++ setup.py
@@ -3,7 +3,17 @@
 # See http://pythonhosted.org/setuptools/setuptools.html#development-mode
 # See https://github.com/scikit-learn/scikit-learn/issues/1016
 import os
+import platform
 from setuptools import setup
+
+uname = platform.uname()
+system_name = uname[0]
+if system_name == "Windows":
+    script_extension = ".exe"
+elif system_name == "Darwin":
+    script_extension = ".osx"
+elif system_name == "Linux":
+    script_extension = ".linux"
 
 # See https://github.com/warner/python-versioneer
 import imp
@@ -43,6 +53,9 @@ setup(name='brownian',
       'nose', 'h5py', 'uncertainties', 'click', 'BeautifulSoup4', 'docutils',
       'bunch', 'six', 'psutil', 'kpfm'
       ],
+      package_data=['brownian': ['stanmodels/gamma{}'.format(script_extension),
+                                  'stanmodels/gamma2{}'.format(script_extension),
+                                  'ex/brownian-red.h5']
       tests_require=['nose'],
       test_suite='brownian.tests.discover',
       license = 'MIT',
